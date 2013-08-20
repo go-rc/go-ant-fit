76bf43bcb8ae46e9be19fb46109db49f6509f613
Dave Glowacki <dave@glowacki.org>
2013-08-20 09:46:39 -0500

    Can now read entire file

--- readfit.go
+++ readfit.go
@@ -1,15 +1,32 @@
 package main
 
 import (
-    "encoding/binary"
     "bufio"
     "bytes"
+    "encoding/binary"
     "errors"
     "flag"
     "fmt"
     "io"
     "os"
 )
+
+var base_type_names = [14]string{
+    "enum",
+    "int8",
+    "uint8",
+    "int16",
+    "uint16",
+    "int32",
+    "uint32",
+    "string",
+    "float32",
+    "float64",
+    "uint8z",
+    "uint16z",
+    "uint32z",
+    "byte",
+}
 
 func addCRC(crc uint16, val byte) uint16 {
     lookup := [16]uint16{
@@ -38,9 +55,9 @@ func checkCRC(rdr io.Reader, data []byte) error {
     n, err := rdr.Read(buf)
     if err != nil {
         return err
-    } else if n != 2 {
-        errfmt := "Tried to read 2 byte CRC, only read %d bytes"
-        return errors.New(fmt.Sprintf(errfmt, n))
+    } else if n != len(buf) {
+        errfmt := "Tried to read %d byte CRC, only read %d bytes"
+        return errors.New(fmt.Sprintf(errfmt, len(buf), n))
     }
 
     goodCRC := to_uint16(buf)
@@ -62,30 +79,73 @@ func checkCRC(rdr io.Reader, data []byte) error {
     return nil
 }
 
-type FitData struct {
+type FitFile struct {
     filename string
     rdr io.Reader
 
     proto byte
     profile uint16
     datasize uint32
+
+    defs []*FitDefinition
+    data []*FitData
 }
 
-func (fdata *FitData) open(filename string) error {
+type FitFieldDefinition struct {
+    num byte
+    size byte
+    is_endian bool
+    base_type byte
+}
+
+type FitDefinition struct {
+    local_type byte
+    little_endian bool
+    global_num uint16
+    fields []*FitFieldDefinition
+    total_bytes uint16
+}
+
+func (s ByNum) Less(i, j int) bool {
+    return s.Definitions[i].num < s.Definitions[j].num
+}
+
+type FitData struct {
+    values []*interface{}
+}
+
+type MsgFileId struct {
+    msgtype byte
+    manufacturer uint16
+    product uint16
+    serial_number uint32
+    time_created uint32
+    number uint16
+}
+
+func (msg *MsgFileId) name() string {
+    return "file_id"
+}
+
+func createFileId(data []byte) *MsgFileId {
+    return nil
+}
+
+func (ffile *FitFile) open(filename string) error {
     file, err := os.Open(filename)
     if err != nil {
         return errors.New(fmt.Sprintf("Cannot open \"%s\"\n", filename))
     }
     defer file.Close()
 
-    fdata.filename = filename
-    fdata.rdr = bufio.NewReader(file)
+    ffile.filename = filename
+    ffile.rdr = bufio.NewReader(file)
 
     const minHeaderLen byte = 12
 
     buf := make([]byte, minHeaderLen)
 
-    n, err := fdata.rdr.Read(buf)
+    n, err := ffile.rdr.Read(buf)
     if err != nil {
         return err
     } else if n != int(minHeaderLen) {
@@ -112,42 +172,217 @@ func (fdata *FitData) open(filename string) error {
 
     // verify that the CRC is correct (if present)
     if needCRC {
-        err = checkCRC(fdata.rdr, buf)
+        err = checkCRC(ffile.rdr, buf)
         if err != nil {
             return err
         }
     }
 
-    fdata.proto = buf[1]
-    fdata.profile = to_uint16(buf[2:4])
-    fdata.datasize = to_uint32(buf[4:9])
+    ffile.proto = buf[1]
+    ffile.profile = to_uint16(buf[2:4])
+    ffile.datasize = to_uint32(buf[4:9])
 
+    ffile.defs = make([]*FitDefinition, 0)
+    ffile.data = make([]*FitData, 0)
     return nil
 }
 
-func readFit(filename string) error {
-    fdata := new(FitData)
+func (ffile *FitFile) findDefinition(local_type byte) (*FitDefinition, error) {
+    var def *FitDefinition
+    for i := 0; i < len(ffile.defs); i++ {
+        if ffile.defs[i].local_type == local_type {
+            def = ffile.defs[i]
+        }
+    }
 
-    err := fdata.open(filename)
+    var err error
+    if def == nil {
+        err = errors.New(fmt.Sprintf("Unknown local_type %d", local_type))
+    }
+
+    return def, err
+}
+
+func (ffile *FitFile) readData(def *FitDefinition,
+    time_offset uint32, verbose bool) (*FitData, error) {
+
+    buf := make([]byte, def.total_bytes)
+
+    n, err := ffile.rdr.Read(buf)
+    if err != nil {
+        return nil, err
+    } else if n != len(buf) {
+        return nil, errors.New(fmt.Sprintf("Read %d bytes, not %d", n,
+            len(buf)))
+    }
+
+    data := new(FitData)
+
+    fmt.Printf("fmt.readData UNIMPLEMENTED for def %d\n", def.local_type)
+
+    return data, nil
+}
+
+func (ffile *FitFile) readDefinition(local_type byte,
+    verbose bool) (*FitDefinition, error) {
+    buf := make([]byte, 5)
+
+    n, err := ffile.rdr.Read(buf)
+    if err != nil {
+        return nil, err
+    } else if n != len(buf) {
+        return nil, errors.New(fmt.Sprintf("Read %d bytes, not %d", n,
+            len(buf)))
+    }
+
+    def := new(FitDefinition)
+
+    def.local_type = local_type
+    def.little_endian = buf[1] == 0
+    def.global_num = to_uint16(buf[2:4])
+    def.total_bytes = 0
+
+    num := int(buf[4])
+
+    def.fields = make([]*FitFieldDefinition, num)
+    for i := 0; i < num; i++ {
+        def.fields[i], err = ffile.readFieldDef(buf)
+        if err != nil {
+            return nil, err
+        }
+        def.total_bytes += uint16(def.fields[i].size)
+    }
+
+    if verbose {
+        fmt.Printf("  def: ltyp %v little_endian %v glbl %d\n",
+            def.local_type, def.little_endian, def.global_num)
+        for i := 0; i < len(def.fields); i++ {
+            var type_name string
+
+            if def.fields[i].base_type >= 0 &&
+                int(def.fields[i].base_type) < len(base_type_names) {
+                type_name = base_type_names[def.fields[i].base_type]
+            } else {
+                type_name = fmt.Sprintf("?? %d ??", def.fields[i].base_type)
+            }
+            fmt.Printf("       :: num %d sz %d endian %v type %s\n",
+                def.fields[i].num, def.fields[i].size, def.fields[i].is_endian,
+                type_name)
+        }
+    }
+
+    return def, nil
+}
+
+func (ffile *FitFile) readFieldDef(buf []byte) (*FitFieldDefinition, error) {
+    n, err := ffile.rdr.Read(buf[:3])
+    if err != nil {
+        return nil, err
+    } else if n != 3 {
+        return nil, errors.New(fmt.Sprintf("Read %d bytes, not %d", n, 3))
+    }
+
+    fld := new(FitFieldDefinition)
+
+    fld.num = buf[0]
+    fld.size = buf[1]
+    fld.is_endian = buf[2] & 0x80 == 0x80
+    fld.base_type = buf[2] & 0xf
+
+    return fld, nil
+}
+
+func (ffile *FitFile) readMessage(verbose bool) (bool, error) {
+    buf := make([]byte, 1)
+
+    n, err := ffile.rdr.Read(buf)
+    if err != nil {
+        return false, err
+    } else if n != len(buf) {
+        return false, errors.New(fmt.Sprintf("Read %d bytes, not %d", n,
+            len(buf)))
+    }
+
+    var is_def bool
+    var local_type byte
+    var time_offset uint32
+
+    compressed := buf[0] & 0x80 == 0x80
+    if !compressed {
+        is_def = buf[0] & 0x40 == 0x40
+        local_type = buf[0] & 0x0f
+        time_offset = 0
+    } else {
+        is_def = false
+        local_type = (buf[0] >> 4) & 0x3
+        time_offset = uint32(buf[0] & 0xf)
+    }
+
+    if is_def {
+        def, derr := ffile.readDefinition(local_type, verbose)
+        if derr != nil {
+            return false, derr
+        }
+
+        ffile.defs = append(ffile.defs, def)
+    } else {
+        def, err2 := ffile.findDefinition(local_type)
+        if err2 != nil {
+            return false, err2
+        }
+
+        data, err3 := ffile.readData(def, time_offset, verbose)
+        if err3 != nil {
+            return false, err3
+        }
+        fmt.Println("!! Discarding data ", data);
+    }
+
+    return true, nil
+}
+
+func readFit(filename string, verbose bool) error {
+    ffile := new(FitFile)
+
+    err := ffile.open(filename)
     if err != nil {
         return err
     }
 
-    fmt.Printf("%s: proto %d profile %d data %d\n", fdata.filename,
-        fdata.proto, fdata.profile, fdata.datasize)
+    if verbose {
+        fmt.Printf("%s: proto %d profile %d data %d\n", ffile.filename,
+            ffile.proto, ffile.profile, ffile.datasize)
+    }
+
+    for true {
+        flag, err := ffile.readMessage(verbose)
+        if err != nil {
+            return err
+        } else if !flag {
+            break
+        }
+    }
 
     return nil
 }
 
 func to_uint16(data []byte) (ret uint16) {
+    return to_uint16_endian(data, binary.LittleEndian)
+}
+
+func to_uint16_endian(data []byte, order binary.ByteOrder) (ret uint16) {
     buf := bytes.NewBuffer(data)
-    binary.Read(buf, binary.LittleEndian, &ret)
+    binary.Read(buf, order, &ret)
     return
 }
 
 func to_uint32(data []byte) (ret uint32) {
+    return to_uint32_endian(data, binary.LittleEndian)
+}
+
+func to_uint32_endian(data []byte, order binary.ByteOrder) (ret uint32) {
     buf := bytes.NewBuffer(data)
-    binary.Read(buf, binary.LittleEndian, &ret)
+    binary.Read(buf, order, &ret)
     return
 }
 
@@ -183,12 +418,8 @@ func processArgs() (bool, []string) {
 func main() {
     verbose, files := processArgs()
 
-    if verbose {
-        fmt.Println("Verbose mode")
-    }
-
     for _, f := range files {
-        err := readFit(f)
+        err := readFit(f, verbose)
         if err != nil {
             fmt.Printf("!! Cannot read %s: %s\n", f, err)
         }
